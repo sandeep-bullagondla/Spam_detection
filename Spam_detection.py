3b2f87b84dcf8978bfdc3e20c798129b20cd079f
sandeep-bullagondla <sandeep.bullagondla@gmail.com>
2023-02-15 09:26:11 +0000

    Spam detection using Navie Bayes

---
+++ Spam_detection.py
@@ -0,0 +1,36 @@
+import pandas as pd 
+import numpy as np 
+from sklearn.model_selection import train_test_split 
+from sklearn.feature_extraction.text import CountVectorizer 
+from sklearn.naive_bayes import MultinomialNB 
+
+data = pd.read_csv("https://raw.githubusercontent.com/amankharwal/SMS-Spam-Detection/master/spam.csv", encoding= 'latin-1')
+data = data[["class", "message"]] 
+
+X = np.array(data['message']) 
+y = np.array(data['class']) 
+
+cv = CountVectorizer() 
+X = cv.fit_transform(X) 
+
+trainX, testX, trainY, testY = train_test_split(X, y, test_size = 0.33, random_state = 42) 
+
+clf = MultinomialNB().fit(trainX, trainY) 
+
+import streamlit as st 
+
+st.title("Spam Detection system") 
+
+def spam_detection(): 
+    user = st.text_area("Enter any message or mail:" ) 
+    if len(user)<1: 
+        st.write(" ") 
+    
+    else: 
+        sample = user 
+        data = cv.transform([sample]).toarray()
+        a = clf.predict(data) 
+        st.title(a)
+
+spam_detection()
+
